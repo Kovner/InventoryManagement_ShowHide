5a48122dc75d54a363aaceff44d7d4791ba90bfe
Michael Kovner <mkovner@tableau.com>
2018-10-16 14:47:18 -0700

    Use tableau-ui and other css changes

--- public/js/inventory-management.js
+++ public/js/inventory-management.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  //Render tableau-ui React components
+  ReactDOM.render(
+    React.createElement(TableauUI.Button, {buttonType: 'filledGreen'}, "Update"), 
+    document.getElementById('updateItem')
+  );
+
   let worksheet;
   const wsName = 'Inventory';    // This is the sheet we'll use for updating task info
 
@@ -57,11 +63,11 @@ $(document).ready(function () {
 
   function populateTable (dt) {
     $('#data_table tr').remove();
-    var headerRow = $('<tr/>');
-    headerRow.append('<th>Product</th>');
-    headerRow.append('<th>Stock</th>');
-    headerRow.append('<th>Ordered</th>');
-    $('#data_table').append(headerRow);
+    // var headerRow = $('<tr/>');
+    // headerRow.append('<th>Product</th>');
+    // headerRow.append('<th>Stock</th>');
+    // headerRow.append('<th>Ordered</th>');
+    // $('#data_table').append(headerRow);
 
     let productIndex, stockIndex, orderedIndex, rowIDIndex;
 
@@ -90,9 +96,37 @@ $(document).ready(function () {
       const rowID = item[rowIDIndex].formattedValue;
       let dataRow = $('<tr/>');
       dataRow.append('<td>' + item[productIndex].formattedValue + '</td>');
-      dataRow.append('<td><input type="text" size="8" id="row_' + rowID + '_stock" value="' + item[stockIndex].formattedValue + '" /></td>');
-      dataRow.append('<td><input type="text" size="8" id="row_' + rowID + '_ordered" value="' + item[orderedIndex].formattedValue + '" /></td>');
+      // dataRow.append('<td><input type="text" size="8" id="row_' + rowID + '_stock" value="' + item[stockIndex].formattedValue + '" /></td>');
+      // dataRow.append('<td><input type="text" size="8" id="row_' + rowID + '_ordered" value="' + item[orderedIndex].formattedValue + '" /></td>');
+      dataRow.append('<td><div class="inputContainer" id="row_' + rowID + '_stock"/></td>');
+      dataRow.append('<td><div class="inputContainer" id="row_' + rowID + '_ordered"/></td>');
       $('#data_table').append(dataRow);
+
+      //Render the React Text Inputs
+      const stockProps = {
+        // onChange: e => this.setState({value: e.target.value, error: e.target.value === 'error' && 'Lorem'}),
+        // onClear: () => this.setState({value: ''}),
+        label: 'Stock',
+        style: { width: 140 },
+        styleType: 'line',
+        defaultValue: item[stockIndex].formattedValue,
+      };
+      ReactDOM.render(
+        React.createElement(TableauUI.TextField, stockProps),
+        document.getElementById('row_' + rowID + '_stock')
+      )
+      const orderedProps = {
+        // onChange: e => this.setState({value: e.target.value, error: e.target.value === 'error' && 'Lorem'}),
+        // onClear: () => this.setState({value: ''}),
+        label: 'Ordered',
+        style: { width: 140 },
+        styleType: 'line',
+        defaultValue: item[orderedIndex].formattedValue,
+      };
+      ReactDOM.render(
+        React.createElement(TableauUI.TextField, orderedProps),
+        document.getElementById('row_' + rowID + '_ordered')
+      )
     });
     let dashboard = tableau.extensions.dashboardContent.dashboard;
     const visibilityMap = {7: tableau.ZoneVisibilityType.Show};
@@ -127,3 +161,43 @@ $(document).ready(function () {
     // event.preventDefault();
   });
 });
+
+
+function submitData() {
+  // let formInputs = $('form#projectTasks :input[type="text"]');
+  // let postData = [];
+
+  // formInputs.each(function () {
+  //   let c = $(this);
+  //   postData.push({id: c[0].id, 'value': c[0].value});
+  // });
+  let postData = [];
+
+  let $inputArray = $('.inputContainer');
+  // $inputArray.each(function(inputDiv) {
+  for(let inputDiv of $inputArray) {
+    let inputData = {};
+    inputData['id'] = inputDiv.id;
+    let inputText = $(inputDiv).find('input')[0];
+    inputData['value'] = inputText.value;
+    postData.push(inputData);
+  };
+
+  let dashboard = tableau.extensions.dashboardContent.dashboard;
+  let worksheet = dashboard.worksheets.find( ws => ws.name === "Inventory");
+
+  // Post it
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:8765',
+    data: JSON.stringify(postData),
+    contentType: 'application/json'
+  }).done(
+    worksheet.getDataSourcesAsync().then(function (dataSources) {
+      dataSources[0].refreshAsync();
+    })
+  );
+  const visibilityMap = {7: tableau.ZoneVisibilityType.Hide};
+  dashboard.setZoneVisibilityAsync(visibilityMap);
+
+}
